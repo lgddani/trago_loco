611795b4e4de6bece912293875057ade2a3a9c36
Daniel Enriquez <rdanienriquez01@gmail.com>
2025-04-26 12:53:11 -0500

    Creación del inicio de la aplicación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComHomeComponent } from './home/com-home/com-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ComHomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/home/com-home/com-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-com-home',
+  standalone: false,
+  templateUrl: './com-home.component.html',
+  styleUrl: './com-home.component.css'
+})
+export class ComHomeComponent {
+
+}
